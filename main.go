e57427d2d54262981bce8d68d96ecdc9130b4128
andrey <dovban.andreyy.rambler.ru>
2025-08-31 09:11:56 +0300

    fix

--- main.go
+++ main.go
@@ -3,10 +3,6 @@ package main
 
 import (
 	"embed"
-	"fmt"
-	"test/configs"
-	"test/connect"
-	"test/store"
 
 	// "fmt"
 	"net/http"
@@ -17,53 +13,53 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
 )
 
-//go:embed public/*
-var public embed.FS
+//go:embed dist/*
+var dist embed.FS
 
 func main() {
-	config := configs.LoadConfig()
+	// config := configs.LoadConfig()
 
 	// Создаем новый экземпляр Fiber
 	app := fiber.New()
 
 	app.Use("/", filesystem.New(filesystem.Config{
-		Root:       http.FS(public),
-		PathPrefix: "public",
+		Root:       http.FS(dist),
+		PathPrefix: "dist",
 		Browse:     true,
 	}))
 
-	conn, err := connect.LdapAuth(config.Server, config.Port, config.Login, config.Password)
-	if err != nil {
-		fmt.Println("Auth Error", err.Error())
-		conn.Close()
-	}
+	// conn, err := connect.LdapAuth(config.Server, config.Port, config.Login, config.Password)
+	// if err != nil {
+	// 	fmt.Println("Auth Error", err.Error())
+	// 	conn.Close()
+	// }
 
-	defer conn.Close()
+	// defer conn.Close()
 
 	// Определяем обработчик для корневого маршрута
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Привет, Fiber!")
 	})
 
-	app.Get("/api/schema", func(c *fiber.Ctx) error {
-		schema, err := connect.GetSchema(conn, store.BaseDn)
-		if err != nil {
-			fmt.Println("Get Schema Error", err.Error())
-			conn.Close()
-		}
+	// app.Get("/api/schema", func(c *fiber.Ctx) error {
+	// 	schema, err := connect.GetSchema(conn, store.BaseDn)
+	// 	if err != nil {
+	// 		fmt.Println("Get Schema Error", err.Error())
+	// 		conn.Close()
+	// 	}
 
-		return c.Status(fiber.StatusOK).JSON(schema)
-	})
+	// 	return c.Status(fiber.StatusOK).JSON(schema)
+	// })
 
-	app.Get("/api/subschema", func(c *fiber.Ctx) error {
-		schema, err := connect.GetSubSchemaDn(conn, "")
-		if err != nil {
-			fmt.Println("Get SubSchema Dn Error", err.Error())
-			conn.Close()
-		}
+	// app.Get("/api/subschema", func(c *fiber.Ctx) error {
+	// 	schema, err := connect.GetSubSchemaDn(conn, "")
+	// 	if err != nil {
+	// 		fmt.Println("Get SubSchema Dn Error", err.Error())
+	// 		conn.Close()
+	// 	}
 
-		return c.Status(fiber.StatusOK).JSON(schema)
-	})
+	// 	return c.Status(fiber.StatusOK).JSON(schema)
+	// })
 
 	app.Get("/api/test", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).SendFile("./data.json")
